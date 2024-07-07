2e45e1d01cccd5e53afe6a819cceb6daeb98259e
Dezso Binder <binderdezso97@gmail.com>
2024-07-07 19:59:10 +0200

    Cleanup, adjust hero section folder structure

--- frontend/ui/app/[locale]/home/page.tsx
+++ frontend/ui/app/[locale]/home/page.tsx
@@ -1,14 +1,12 @@
-import styles from "./home.module.css";
-import HeroSection from "./HeroSection";
+import HeroSection from "./(hero)/HeroSection";
 import { useTranslations } from "next-intl";
-import LanguagePicker from "@/components/language/LanguagePicker";
 import ProductSection from "./(products)/ProductSection";
 
 export default function Home() {
   const t = useTranslations();
 
   return (
-    <main className={styles.home}>
+    <main>
       <HeroSection />
       <ProductSection />
     </main>
