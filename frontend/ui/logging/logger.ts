26005c802b1879b019cd6ebe05eeb965f6ec39c7
Dezso Binder <binderdezso97@gmail.com>
2024-07-17 09:36:18 +0200

    Add Winston logger
    Configure logger and specify build platform for production server

---
+++ frontend/ui/logging/logger.ts
@@ -0,0 +1,35 @@
+import winston, { createLogger, format, transports } from "winston";
+const { combine, printf, colorize } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  level: "info",
+  format: combine(
+      winston.format.timestamp({
+            format: "YYYY-MM-DD HH:mm:ss"
+        }),
+        winston.format.simple(),
+        winston.format.align(),
+    logFormat
+  ),
+  transports: [
+    new transports.Console({format: combine(
+        colorize(),
+        logFormat
+      )}),
+    new transports.File({ filename: "combined-UI.log", dirname: "logs", maxsize: 5 * 1024 * 1024, // 5 MB each file
+         zippedArchive: true, maxFiles: 500, tailable: true, level: "debug"}),
+    new transports.File({ filename: "error-UI.log", dirname: "logs", maxsize: 5 * 1024 * 1024, // 5 MB each file
+         zippedArchive: true, maxFiles: 500, tailable: true, level: "error"})
+  ]
+});
+
+// Add a listener to log errors from Winston itself
+logger.on("error", (error) => {
+  console.error("Error in Winston logger:", error);
+});
+
+export default logger;
