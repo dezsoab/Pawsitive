0234e24f6df0c89d49d3284db72cfa6ce80425b3
Dezso Binder <binderdezso97@gmail.com>
2024-07-21 18:12:03 +0200

    Add footer section

--- frontend/ui/enums/navigationRoutes.ts
+++ frontend/ui/enums/navigationRoutes.ts
@@ -2,5 +2,10 @@ export enum navigationRoutes {
     ABOUT="/about",
     HOME="/home",
     CONTACT="/contact",
-    SHOP="/shop"
+    SHOP="/shop",
+    PRIVACY="/privacy-policy",
+    TERMS="/terms-and-conditions",
+    FAQs="/FAQs",
+    INSTA="/insta",
+    FACEBOOK="/facebook"
 }
