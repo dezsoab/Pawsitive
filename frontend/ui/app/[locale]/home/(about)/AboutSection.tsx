e3183afbe1b2b967de5c58fc111db7c4855245a1
Dezso Binder <binderdezso97@gmail.com>
2024-07-22 00:55:55 +0200

    Fix about section

--- frontend/ui/app/[locale]/home/(about)/AboutSection.tsx
+++ frontend/ui/app/[locale]/home/(about)/AboutSection.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useTranslations } from "next-intl";
-import styles from "./AboutSection.module.css";
 import Image from "next/image";
+
+import styles from "./AboutSection.module.css";
 import DesignIcon from "./DesignIcon";
-import SafetyIcon  from "./SafetyIcon";
+import SafetyIcon from "./SafetyIcon";
 import CalmIcon from "./CalmIcon";
 
 const AboutSection = () => {
@@ -14,34 +15,43 @@ const AboutSection = () => {
       <div className={styles.aboutContainer}>
         <section>
           <h1>{t("Index.about.about_heading")}</h1>
-          <div className={styles.textFormating}>{t("Index.about.about_description")}</div>
+          <p className={styles.textFormating}>
+            {t("Index.about.about_description")}
+          </p>
         </section>
         <section>
-          <Image src={"/assets/cat_about.png"} alt={"White cat"} width={600} height={450} />
+          <Image
+            src={"/assets/cat_about.png"}
+            alt={"White cat"}
+            width={600}
+            height={450}
+          />
         </section>
       </div>
       <div className={styles.featureContainer}>
-        <section className={styles.featureItem}>
-          <DesignIcon/>
-          <div className={styles.featureText}>
-            <h2>{t("Index.about.first_title")}</h2>
-            <div className={styles.preLine}>{t("Index.about.block_1")}</div>
-          </div>
-        </section>
-        <section className={styles.featureItem}>
-          <SafetyIcon/>
-          <div className={styles.featureText}>
-            <h2>{t("Index.about.second_title")}</h2>
-            <div className={styles.preLine}>{t("Index.about.block_2")}</div>
-          </div>
-        </section>
-        <section className={styles.featureItem}>
-          <CalmIcon/>
-          <div className={styles.featureText}>
-            <h2>{t("Index.about.third_title")}</h2>
-            <div className={styles.preLine}>{t("Index.about.block_2")}</div>
-          </div>
-        </section>
+        <div>
+          <section className={styles.featureItem}>
+            <DesignIcon />
+            <div className={styles.featureText}>
+              <h2>{t("Index.about.first_title")}</h2>
+              <p>{t("Index.about.block_1")}</p>
+            </div>
+          </section>
+          <section className={styles.featureItem}>
+            <SafetyIcon />
+            <div className={styles.featureText}>
+              <h2>{t("Index.about.second_title")}</h2>
+              <p>{t("Index.about.block_2")}</p>
+            </div>
+          </section>
+          <section className={styles.featureItem}>
+            <CalmIcon />
+            <div className={styles.featureText}>
+              <h2>{t("Index.about.third_title")}</h2>
+              <p>{t("Index.about.block_2")}</p>
+            </div>
+          </section>
+        </div>
       </div>
     </section>
   );
--- frontend/ui/app/[locale]/home/page.tsx
+++ frontend/ui/app/[locale]/home/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
+      <AboutSection />
       <ProductSection />
-      <AboutSection/>
       <Testimonial />
       <Reasoning />
       <CTASection />
