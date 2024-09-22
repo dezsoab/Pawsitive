6fdad6b1c9c3f467cd9d83e8a3038eb92cd88249
Dezso Binder <binderdezso97@gmail.com>
2024-09-22 23:11:00 +0200

    Add 'features' and 'services' to landing page

---
+++ frontend/ui/app/[locale]/home/(features)/Features.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StaticImageData } from "next/image";
+
+import FeatureBlock from "./FeatureBlock";
+
+interface FeaturesProps {
+  bgc?: "var(--color-white)" | "var(--color-pink-light)";
+  flexDir?: "row" | "row-reverse" | "column" | "column-reverse";
+  imgPath: StaticImageData;
+  isServices: boolean;
+}
+
+const Features = ({ bgc, flexDir, imgPath, isServices }: FeaturesProps) => {
+  return (
+    <section style={{ backgroundColor: bgc }}>
+      <FeatureBlock
+        flexDir={flexDir}
+        imgPath={imgPath}
+        isServices={isServices}
+      />
+    </section>
+  );
+};
+
+export default Features;
