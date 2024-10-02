5f255b34f97c3d8851beffa124bcabfa17f5fec9
Dezso Binder <binderdezso97@gmail.com>
2024-10-02 22:03:19 +0200

    Add scanned profile page

---
+++ frontend/ui/app/[locale]/pet/[tagId]/page.tsx
@@ -0,0 +1,35 @@
+import Navbar from "@/components/navigation/Navbar";
+import Image from "next/image";
+import React from "react";
+
+import styles from "./ScannedPetProfile.module.css";
+import molli from "../../../../public/assets/molli2.webp";
+import Ribbon from "@/components/ribbon/Ribbon";
+
+const ScannedPetProfile = () => {
+  return (
+    <>
+      <Navbar style={{ backgroundColor: "var(--color-green)" }} />
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <div className={styles.imgContainer}>
+            <Image
+              src={molli}
+              alt="NotFound.molli_image_alt"
+              fill
+              //   objectFit="contain"
+              objectFit="cover"
+            />
+          </div>
+          <div className={styles.petDetails}>
+            <div className={styles.petContent}>
+              <Ribbon name="Molli" />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default ScannedPetProfile;
---
+++ frontend/ui/components/ribbon/Ribbon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./Ribbon.module.css";
+
+interface Props {
+  name: string;
+}
+
+const Ribbon = ({ name }: Props) => {
+  return (
+    <div className={styles.ribbon}>
+      {name}
+      <i></i>
+      <i></i>
+      <i></i>
+      <i></i>
+    </div>
+  );
+};
+
+export default Ribbon;
