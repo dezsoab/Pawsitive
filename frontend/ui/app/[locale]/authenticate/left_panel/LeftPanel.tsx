13a4443210f6e7ba76983904a105b4939a9dda61
Dezso Binder <binderdezso97@gmail.com>
2024-12-18 16:14:22 +0100

    Animate form divider waves
    - set container max-width global variable to 1250px
    -- change hard coded occurences to variable

--- frontend/ui/app/[locale]/authenticate/left_panel/LeftPanel.tsx
+++ frontend/ui/app/[locale]/authenticate/left_panel/LeftPanel.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 import styles from "./LeftPanel.module.css";
+import FormDivider from "@/components/form_divider/FormDivider";
 
 const LeftPanel = () => {
-  return <div className={styles.panel}>LeftPanel</div>;
+  return (
+    <div className={styles.panel}>
+      LeftPanel
+      <FormDivider />
+    </div>
+  );
 };
 
 export default LeftPanel;
