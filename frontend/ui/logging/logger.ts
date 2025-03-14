4efe09757c813966ffdb89ea3a01bcaa6a81829a
Dezso Binder <binderdezso97@gmail.com>
2025-03-14 21:56:38 +0100

    Disable file logging on Vercel

--- frontend/ui/logging/logger.ts
+++ frontend/ui/logging/logger.ts
@@ -5,26 +5,43 @@ const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
 
+const isVercel = process.env.VERCEL === "1";
+
 const logger = createLogger({
   level: "info",
   format: combine(
-      winston.format.timestamp({
-            format: "YYYY-MM-DD HH:mm:ss"
-        }),
-        winston.format.simple(),
-        winston.format.align(),
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.simple(),
+    winston.format.align(),
     logFormat
   ),
   transports: [
-    new transports.Console({format: combine(
-        colorize(),
-        logFormat
-      )}),
-    new transports.File({ filename: "combined-UI.log", dirname: "logs", maxsize: 5 * 1024 * 1024, // 5 MB each file
-         zippedArchive: true, maxFiles: 500, tailable: true, level: "debug"}),
-    new transports.File({ filename: "error-UI.log", dirname: "logs", maxsize: 5 * 1024 * 1024, // 5 MB each file
-         zippedArchive: true, maxFiles: 500, tailable: true, level: "error"})
-  ]
+    new transports.Console({ format: combine(colorize(), logFormat) }),
+    ...(isVercel
+      ? []
+      : [
+          new transports.File({
+            filename: "combined-UI.log",
+            dirname: "logs",
+            maxsize: 5 * 1024 * 1024, // 5 MB each file
+            zippedArchive: true,
+            maxFiles: 500,
+            tailable: true,
+            level: "debug",
+          }),
+          new transports.File({
+            filename: "error-UI.log",
+            dirname: "logs",
+            maxsize: 5 * 1024 * 1024, // 5 MB each file
+            zippedArchive: true,
+            maxFiles: 500,
+            tailable: true,
+            level: "error",
+          }),
+        ]),
+  ],
 });
 
 // Add a listener to log errors from Winston itself
