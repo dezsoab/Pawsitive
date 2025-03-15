1eb9c0ab1616429a5741010ebb48e26b2fc65f94
Dezso Binder <binderdezso97@gmail.com>
2025-03-15 21:07:20 +0100

    Add further logging

--- backend/pawsitive/src/main/java/com/pawsitive/pawsitive/constants/PublicEndpoints.java
+++ backend/pawsitive/src/main/java/com/pawsitive/pawsitive/constants/PublicEndpoints.java
@@ -1,10 +1,17 @@
 package com.pawsitive.pawsitive.constants;
 
+import com.pawsitive.pawsitive.nfctag.controller.NfcTagController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public final class PublicEndpoints {
+    private static final Logger logger = LoggerFactory.getLogger(PublicEndpoints.class);
+
+
     private static final Set<String> ENDPOINTS = new HashSet<>();
 
     public static final String LOGIN = "/api/v1/auth/login";
@@ -21,6 +28,9 @@ public final class PublicEndpoints {
     }
 
     public static boolean isPublicPath(String requestURI) {
+        logger.info("Checking if endpoint - {} - is public", requestURI);
+        logger.info("Collected endpoints: {}", ENDPOINTS);
+
         return matchesPublicUrl(requestURI, "^/api/v1/nfcTag/[a-zA-Z0-9]+$") ||
                 matchesPublicUrl(requestURI, "^/api/v1/pet/[a-zA-Z0-9]+$") ||
                 ENDPOINTS.contains(requestURI);
