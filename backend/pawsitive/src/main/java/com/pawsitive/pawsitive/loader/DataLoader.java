8b0750db173a43cde57ede9f0282221584654dfa
Dezso Binder <binderdezso97@gmail.com>
2025-03-15 10:38:23 +0100

    Add backend docker setup and CI/CD

--- backend/pawsitive/src/main/java/com/pawsitive/pawsitive/loader/DataLoader.java
+++ backend/pawsitive/src/main/java/com/pawsitive/pawsitive/loader/DataLoader.java
@@ -12,8 +12,10 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("test")
 @Component
 @AllArgsConstructor
 public class DataLoader implements CommandLineRunner {
@@ -28,104 +30,115 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) {
         logger.info("Starting data loading...");
 
-        User user1 = User.builder()
-                .email("binderdezso97@gmail.com")
-                .password("v")
-                .active(true)
-                .build();
+        if (userService.countUsers() > 0) {
+            logger.info("Database is NOT empty. Skipping test data loading.");
+            return;
+        }
 
-        Address address1 = Address.builder()
-                .country("Austria")
-                .city("Graz")
-                .zipCode("1111")
-                .street("Test str")
-                .build();
+        logger.info("Database is empty. Starting data loading...");
 
-        Owner owner1 = Owner.builder()
-                .firstName("Dezso")
-                .lastName("Binder")
-                .phone("+123456789")
-                .user(user1)
-                .address(address1)
-                .build();
+        try {
+            User user1 = User.builder()
+                    .email("binderdezso97@gmail.com")
+                    .password("v")
+                    .active(true)
+                    .build();
 
-        user1.setOwner(owner1);
+            Address address1 = Address.builder()
+                    .country("Austria")
+                    .city("Graz")
+                    .zipCode("1111")
+                    .street("Test str")
+                    .build();
 
-        userService.registerUser(user1);
-        logger.info("First user and owner created: User={}, Owner={}", user1, owner1);
+            Owner owner1 = Owner.builder()
+                    .firstName("Dezso")
+                    .lastName("Binder")
+                    .phone("+123456789")
+                    .user(user1)
+                    .address(address1)
+                    .build();
 
-        Pet pet1 = Pet.builder()
-                .name("Molli")
-                .breed("Border Collie")
-                .age(4)
-                .sex("Female")
-                .owner(owner1)
-                .build();
+            user1.setOwner(owner1);
+            userService.registerUser(user1);
+            logger.info("First user and owner created: User={}, Owner={}", user1, owner1);
 
-        petService.createPet(pet1);
-        logger.info("First pet created: {}", pet1);
+            Pet pet1 = Pet.builder()
+                    .name("Molli")
+                    .breed("Border Collie")
+                    .age(4)
+                    .sex("Female")
+                    .owner(owner1)
+                    .build();
 
-        User user2 = User.builder()
-                .email("cinti.katona@example.com")
-                .password("password123")
-                .active(true)
-                .build();
+            petService.createPet(pet1);
+            logger.info("First pet created: {}", pet1);
 
-        Address address2 = Address.builder()
-                .country("Hungary")
-                .city("Budapest")
-                .zipCode("1051")
-                .street("Example Street")
-                .build();
+            User user2 = User.builder()
+                    .email("cinti.katona@example.com")
+                    .password("password123")
+                    .active(true)
+                    .build();
 
-        Owner owner2 = Owner.builder()
-                .firstName("Cinti")
-                .lastName("Katona")
-                .phone("+36123456789")
-                .user(user2)
-                .address(address2)
-                .build();
+            Address address2 = Address.builder()
+                    .country("Hungary")
+                    .city("Budapest")
+                    .zipCode("1051")
+                    .street("Example Street")
+                    .build();
 
-        user2.setOwner(owner2);
+            Owner owner2 = Owner.builder()
+                    .firstName("Cinti")
+                    .lastName("Katona")
+                    .phone("+36123456789")
+                    .user(user2)
+                    .address(address2)
+                    .build();
 
-        userService.registerUser(user2);
-        logger.info("Second user and owner created: User={}, Owner={}", user2, owner2);
+            user2.setOwner(owner2);
+            userService.registerUser(user2);
+            logger.info("Second user and owner created: User={}, Owner={}", user2, owner2);
 
-        Pet pet2 = Pet.builder()
-                .name("Pille")
-                .breed("Unknown")
-                .age(6)
-                .sex("Female")
-                .owner(owner2)
-                .build();
+            Pet pet2 = Pet.builder()
+                    .name("Pille")
+                    .breed("Unknown")
+                    .age(6)
+                    .sex("Female")
+                    .owner(owner2)
+                    .build();
 
-        petService.createPet(pet2);
-        logger.info("Second pet created: {}", pet2);
+            petService.createPet(pet2);
+            logger.info("Second pet created: {}", pet2);
 
-        NfcTag assignedTag1 = NfcTag.builder()
-                .status("active")
-                .pet(pet1)
-                .tagId("ABC123")
-                .build();
-        nfcTagService.createNfcTag(assignedTag1);
-        logger.info("First assigned nfc tag created: {}", assignedTag1);
+            NfcTag assignedTag1 = NfcTag.builder()
+                    .status("active")
+                    .pet(pet1)
+                    .tagId("ABC123")
+                    .build();
+            nfcTagService.createNfcTag(assignedTag1);
+            logger.info("First assigned nfc tag created: {}", assignedTag1);
 
-        NfcTag assignedTag2 = NfcTag.builder()
-                .status("active")
-                .pet(pet2)
-                .tagId("ABC1234")
-                .build();
-        nfcTagService.createNfcTag(assignedTag2);
-        logger.info("Second assigned nfc tag created: {}", assignedTag2);
+            NfcTag assignedTag2 = NfcTag.builder()
+                    .status("active")
+                    .pet(pet2)
+                    .tagId("ABC1234")
+                    .build();
+            nfcTagService.createNfcTag(assignedTag2);
+            logger.info("Second assigned nfc tag created: {}", assignedTag2);
 
-        NfcTag unAssignedTag = NfcTag.builder()
-                .status("inactive")
-                .pet(null)
-                .tagId("ABC1231")
-                .build();
-        nfcTagService.createNfcTag(unAssignedTag);
-        logger.info("Unassigned nfc tag created: {}", unAssignedTag);
+            NfcTag unAssignedTag = NfcTag.builder()
+                    .status("inactive")
+                    .pet(null)
+                    .tagId("ABC1231")
+                    .build();
+            nfcTagService.createNfcTag(unAssignedTag);
+            logger.info("Unassigned nfc tag created: {}", unAssignedTag);
 
-        logger.info("Data loading completed successfully.");
+            logger.info("Data loading completed successfully.");
+
+        } catch (Exception e) {
+            logger.error("Error occurred during data loading: {}", e.getMessage());
+        }
     }
+
 }
