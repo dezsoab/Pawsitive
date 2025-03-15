02b13ab95096111b72e4a843834df0ade0550296
Dezso Binder <binderdezso97@gmail.com>
2025-03-15 19:53:40 +0100

    Configure TEST and PROD server for cors on backend

--- backend/pawsitive/src/main/java/com/pawsitive/pawsitive/config/WebConfig.java
+++ backend/pawsitive/src/main/java/com/pawsitive/pawsitive/config/WebConfig.java
@@ -1,15 +1,23 @@
 package com.pawsitive.pawsitive.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${CORS_ALLOWED_ORIGINS_TEST}")
+    private String CORS_ALLOWED_ORIGINS_TEST;
+
+    @Value("${CORS_ALLOWED_ORIGINS_PROD}")
+    private String CORS_ALLOWED_ORIGINS_PROD;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://10.0.0.136:3000", "http://localhost:3000")
+                .allowedOrigins("http://10.0.0.136:3000", "http://localhost:3000",
+                        CORS_ALLOWED_ORIGINS_PROD, CORS_ALLOWED_ORIGINS_TEST)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
