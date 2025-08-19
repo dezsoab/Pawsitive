b36494c5f52d2d13f742e41d11375c37645ce63f
Dezso Binder <binderdezso97@gmail.com>
2025-08-19 22:11:16 +0200

    Load contact hero image through nextjs
    - instead of making the image as a css background, optimize load time through loading it with nextjs

--- frontend/ui/app/[locale]/contact/(hero)/Hero.tsx
+++ frontend/ui/app/[locale]/contact/(hero)/Hero.tsx
@@ -5,6 +5,7 @@ import styles from "./Hero.module.css";
 import { useTranslations } from "next-intl";
 import { contacts } from "@/enums/contact";
 import logger from "@/logging/logger";
+import Image from "next/image";
 
 const Hero = () => {
   const t = useTranslations();
@@ -12,6 +13,13 @@ const Hero = () => {
 
   return (
     <section className={styles.hero}>
+      <Image
+        src="/assets/duett1-landscape.webp"
+        alt="Hero image"
+        fill
+        style={{ objectFit: "cover", objectPosition: "center 12%" }}
+        priority
+      />
       <div>
         <h1>{t("Contact.hero.title")}</h1>
         <p>{t("Contact.hero.description")}</p>
