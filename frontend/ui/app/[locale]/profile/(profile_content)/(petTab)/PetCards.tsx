0d6936700b7ce06238a4cebfa427cb78a31559cd
Dezso Binder <binderdezso97@gmail.com>
2025-10-25 00:55:33 +0200

    Add delete pet button; WIP #325

--- frontend/ui/app/[locale]/profile/(profile_content)/(petTab)/PetCards.tsx
+++ frontend/ui/app/[locale]/profile/(profile_content)/(petTab)/PetCards.tsx
@@ -111,6 +111,10 @@ const PetCards = ({ profile, setProfile }: PetCardsProps) => {
     setEditPetId(null);
   };
 
+  const onDeleteHandler = () => {
+    alert("Delete pet functionality to be implemented.");
+  };
+
   return (
     <div>
       {cropModal && (
@@ -193,6 +197,7 @@ const PetCards = ({ profile, setProfile }: PetCardsProps) => {
                       </div>
 
                       <div className={styles.card__infos}>
+                        {/* ================= Edit mode ================= */}
                         {isEditMode ? (
                           <form
                             key={pet.id}
@@ -286,8 +291,25 @@ const PetCards = ({ profile, setProfile }: PetCardsProps) => {
                               </svg>
                               {t("Dashboard.save")}
                             </button>
+
+                            <button
+                              type="button"
+                              className={styles.deleteBtn}
+                              onClick={onDeleteHandler}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                              >
+                                <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z" />
+                              </svg>
+                              {t("Dashboard.delete")}
+                            </button>
                           </form>
                         ) : (
+                          //  ================= Display mode =================
                           <>
                             <p>
                               {t("Pet.name")}: {pet.name}
